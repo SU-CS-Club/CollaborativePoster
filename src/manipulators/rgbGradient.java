3d130d8deadb4121c4e2d120579fe058f20fd1b3
Matthew Volkin <119533382+MattVolkin@users.noreply.github.com>
2023-04-17 13:59:00 +0000

    Rainbow Gradient

---
+++ src/manipulators/rgbGradient.java
@@ -0,0 +1,20 @@
+package manipulators;
+
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * 
+ *
+ * @author Matthew Volkin
+ */
+public class rgbGradient extends Manipulator {
+
+    @Override
+    public Color getColorAtPoint(int x, int y, float brightness, Random random) {
+        float red = ((float)y )/image.getHeight(); 
+        float green  = ((float)x)/image.getWidth();
+        float blue =((float)image.getWidth()-x)/image.getWidth() ;
+        return new Color(red*brightness, green*brightness, blue*brightness); 
+    }
+}

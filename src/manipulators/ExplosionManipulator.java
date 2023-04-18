c81567ac247489a686d22de66ee0a90c63c37eed
leese2 <119525219+leese2@users.noreply.github.com>
2023-04-18 16:03:16 +0000

    blow up the logo - ExplosionManipulator

---
+++ src/manipulators/ExplosionManipulator.java
@@ -0,0 +1,18 @@
+package manipulators;
+import java.awt.Color;
+import java.util.Random;
+
+public class ExplosionManipulator extends ImageManipulator {
+    public ExplosionManipulator() {
+        super("images/explosion.png");
+    }
+    @Override
+    public Color getColorAtPoint(int x, int y, float brightness, Random random) {
+        int option = random.nextInt(3);
+        if (option > 1 && brightness < 0.25){ // Blow up 2/3 of the logo
+            return Color.WHITE;
+        } else { 
+            return super.getColorAtPoint(x,y,brightness,random);
+        }
+    }
+}

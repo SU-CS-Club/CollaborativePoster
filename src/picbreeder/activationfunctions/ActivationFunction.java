5d27dd2f7fff811a3b86918eb06b9914d4440c1b
Jacob Schrum <schrum2@southwestern.edu>
2023-04-22 20:23:33 -0500

    Lot of Picbreeder code added
    Not done yet, but goal is to create a CPPN image

---
+++ src/picbreeder/activationfunctions/ActivationFunction.java
@@ -0,0 +1,23 @@
+package picbreeder.activationfunctions;
+
+public interface ActivationFunction {
+	/**
+	 * Function from real number to real number
+	 * @param x Function input
+	 * @return Result
+	 */
+	public double f(double x);
+	
+	/**
+	 * If there is an equivalent DL4J activation function,
+	 * return the enum entry identifying it.
+	 * @return Instance of Activation enum from DL4J
+	 */
+//	public Activation equivalentDL4JFunction();
+	
+	/**
+	 * Display name for this function
+	 * @return
+	 */
+	public String name();
+}
---
+++ src/picbreeder/activationfunctions/SawtoothFunction.java
@@ -0,0 +1,32 @@
+package picbreeder.activationfunctions;
+
+import picbreeder.ActivationFunctions;
+
+//import org.nd4j.linalg.activations.Activation;
+
+//import edu.southwestern.networks.ActivationFunctions;
+
+public class SawtoothFunction implements ActivationFunction {
+
+	/**
+	 * Sawtooth function for x. mimics sine but in piecewise way. Uses
+	 * Math.floor().
+	 *
+	 * @param x Function parameter
+	 * @return value of sawtooth(x)
+	 */
+	@Override
+	public double f(double x) {
+		return ActivationFunctions.halfSawtooth(x);
+	}
+
+//	@Override
+//	public Activation equivalentDL4JFunction() {
+//		throw new UnsupportedOperationException("No corresponding DL4J function for " + name());
+//	}
+
+	@Override
+	public String name() {
+		return "sawtooth";
+	}
+}
